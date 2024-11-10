1dd6198fef7e3994d942c1e6ece4e24a7930388e
nin8995 <toho.maru9.ggl2@gmail.com>
2024-11-10 17:02:45 +0900

    Mod認識

--- src/main/java/dev/felnull/mekanismtweaks/MekanismTweaks.java
+++ src/main/java/dev/felnull/mekanismtweaks/MekanismTweaks.java
@@ -2,22 +2,27 @@ package dev.felnull.mekanismtweaks;
 
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
 
+import javax.annotation.Nullable;
 import java.io.File;
+import java.util.Map;
 
-/**
- * Config
- */
-@Mod(modid = "mekanismtweaks", name = "MekanismTweaks", version = "1.2", dependencies = "required-after:mekanism")
-public class MekanismTweaks {
+@Mod(modid = "mekanismtweaks")
+@IFMLLoadingPlugin.Name("mekanismtweaks")
+public class MekanismTweaks implements IFMLLoadingPlugin {
 
+    /**
+     * Config
+     */
     public static int maxSpeed;
     public static int maxEnergy;
     public static int freeEnergy;
 
     static {
-        File configFile = new File("config/mekanismtweaks.cfg");//load before MekanismItems class is loaded
+        File configFile = new File("config/mekanismtweaks.cfg");//load before MekanismItems class to change MaxStackSize
         Configuration config = new Configuration(configFile);
         config.load();
         String category = "general";
@@ -30,8 +35,36 @@ public class MekanismTweaks {
         config.save();
     }
 
-    @Mod.EventHandler
-    public void init(FMLPreInitializationEvent event) {
-        System.out.println("Am I there?");
+    /**
+     * Mixin
+     */
+    public MekanismTweaks() {
+        MixinBootstrap.init();
+        MixinEnvironment.getDefaultEnvironment().addConfiguration("mixins.mekanismtweaks.json");
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> map) {
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
     }
 }
